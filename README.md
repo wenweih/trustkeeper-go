acb44c547cd155a9133b9e4ba8421436af9c5ed6
hww <mr.huangwenwei@gmail.com>
2019-04-27 01:53:09 +0800

    Create README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# trustkeeper-go
+币管家后端微服务
--- app/interface/web_api/pkg/service/service.go
+++
@@ -1,7 +0,0 @@
-package service
-
-// WebApiService describes the service.
-type WebApiService interface {
-	// Add your methods here
-	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
-}
