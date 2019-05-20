d182dfdee7cbab0d3866e910e5a64aa0d4259d0d
huangwenwei <mr.huangwenwei@gmail.com>
2019-05-20 15:44:55 +0800

    Add account service grpc client

---
+++ app/service/account/client/grpc/client.go
@@ -0,0 +1,52 @@
+package grpc
+
+import (
+	grpc1 "github.com/go-kit/kit/transport/grpc"
+	endpoint "github.com/go-kit/kit/endpoint"
+	grpc "google.golang.org/grpc"
+	endpoint1 "trustkeeper-go/app/service/account/pkg/endpoint"
+	pb "trustkeeper-go/app/service/account/pkg/grpc/pb"
+	service "trustkeeper-go/app/service/account/pkg/service"
+	// grpctransport "github.com/go-kit/kit/transport/grpc"
+)
+
+// New returns an AddService backed by a gRPC server at the other end
+//  of the conn. The caller is responsible for constructing the conn, and
+// eventually closing the underlying transport. We bake-in certain middlewares,
+// implementing the client library pattern.
+func New(conn *grpc.ClientConn, options []grpc1.ClientOption) (service.AccountService, error) {
+	// grpctransport.ClientBefore()
+	var createEndpoint endpoint.Endpoint
+	{
+		createEndpoint = grpc1.NewClient(conn, "pb.Account", "Create", encodeCreateRequest, decodeCreateResponse, pb.CreateReply{}, options...).Endpoint()
+	}
+
+	var signinEndpoint endpoint.Endpoint
+	{
+		signinEndpoint = grpc1.NewClient(conn, "pb.Account", "Signin", encodeSigninRequest, decodeSigninResponse, pb.SigninReply{}, options...).Endpoint()
+	}
+
+	var signoutEndpoint endpoint.Endpoint
+	{
+		signoutEndpoint = grpc1.NewClient(conn, "pb.Account", "Signout", encodeSignoutRequest, decodeSignoutResponse, pb.SignoutReply{}, options...).Endpoint()
+	}
+
+	var rolesEndpoint endpoint.Endpoint
+	{
+		rolesEndpoint = grpc1.NewClient(conn, "pb.Account", "Roles", encodeRolesRequest, decodeRolesResponse, pb.RolesReply{}, options...).Endpoint()
+	}
+
+	// var findByTokenIDEndpoint endpoint.Endpoint
+	// {
+		// findByTokenIDEndpoint = grpc1.NewClient(conn, "pb.Account", "FindByTokenID", encodeFindByTokenIDRequest, decodeFindByTokenIDResponse, pb.FindByTokenIDReply{}, options["FindByTokenID"]...).Endpoint()
+	// }
+	// grpctransport.NewClient(cc, serviceName, method, enc, dec, grpcReply)
+
+	return endpoint1.Endpoints{
+		CreateEndpoint:        createEndpoint,
+		// FindByTokenIDEndpoint: findByTokenIDEndpoint,
+		RolesEndpoint:         rolesEndpoint,
+		SigninEndpoint:        signinEndpoint,
+		SignoutEndpoint:       signoutEndpoint,
+	}, nil
+}
---
+++ app/service/account/client/grpc/request_response.go
@@ -0,0 +1,86 @@
+package grpc
+
+import (
+  "fmt"
+  "context"
+  "errors"
+  pb "trustkeeper-go/app/service/account/pkg/grpc/pb"
+  "trustkeeper-go/app/service/account/pkg/endpoint"
+)
+
+// encodeCreateRequest is a transport/grpc.EncodeRequestFunc that converts a
+//  user-domain Create request to a gRPC request.
+func encodeCreateRequest(_ context.Context, request interface{}) (interface{}, error) {
+	r := request.(endpoint.CreateRequest)
+	return &pb.CreateRequest{
+		Email: r.Email,
+		Password: r.Password}, nil
+}
+
+// decodeCreateResponse is a transport/grpc.DecodeResponseFunc that converts
+// a gRPC concat reply to a user-domain concat response.
+func decodeCreateResponse(_ context.Context, reply interface{}) (interface{}, error) {
+  _, found := reply.(*pb.CreateReply)
+  if !found{
+    return nil, fmt.Errorf("pb CreateReply type assertion error")
+  }
+  return &endpoint.CreateResponse{E1: nil}, nil
+}
+
+// encodeSigninRequest is a transport/grpc.EncodeRequestFunc that converts a
+//  user-domain Signin request to a gRPC request.
+func encodeSigninRequest(_ context.Context, request interface{}) (interface{}, error) {
+  r := request.(endpoint.SigninRequest)
+  return &pb.SigninRequest{
+    Email: r.Email,
+    Password: r.Password}, nil
+}
+
+// decodeSigninResponse is a transport/grpc.DecodeResponseFunc that converts
+// a gRPC concat reply to a user-domain concat response.
+func decodeSigninResponse(_ context.Context, reply interface{}) (interface{}, error) {
+  r, found := reply.(*pb.SigninReply)
+  if !found {
+    e := fmt.Errorf("pb CreateReply type assertion error")
+    return &endpoint.SigninResponse{
+      E1: e,
+    }, e
+  }
+  return &endpoint.SigninResponse{S0: r.Token,}, nil
+}
+
+// encodeSignoutRequest is a transport/grpc.EncodeRequestFunc that converts a
+//  user-domain Signout request to a gRPC request.
+func encodeSignoutRequest(_ context.Context, request interface{}) (interface{}, error) {
+	return nil, errors.New("'Account' Encoder is not impelemented")
+}
+
+// decodeSignoutResponse is a transport/grpc.DecodeResponseFunc that converts
+// a gRPC concat reply to a user-domain concat response.
+func decodeSignoutResponse(_ context.Context, reply interface{}) (interface{}, error) {
+	return nil, errors.New("'Account' Decoder is not impelemented")
+}
+
+// encodeRolesRequest is a transport/grpc.EncodeRequestFunc that converts a
+//  user-domain Roles request to a gRPC request.
+func encodeRolesRequest(_ context.Context, request interface{}) (interface{}, error) {
+	return nil, errors.New("'Account' Encoder is not impelemented")
+}
+
+// decodeRolesResponse is a transport/grpc.DecodeResponseFunc that converts
+// a gRPC concat reply to a user-domain concat response.
+func decodeRolesResponse(_ context.Context, reply interface{}) (interface{}, error) {
+	return nil, errors.New("'Account' Decoder is not impelemented")
+}
+
+// encodeFindByTokenIDRequest is a transport/grpc.EncodeRequestFunc that converts a
+//  user-domain FindByTokenID request to a gRPC request.
+func encodeFindByTokenIDRequest(_ context.Context, request interface{}) (interface{}, error) {
+	return nil, errors.New("'Account' Encoder is not impelemented")
+}
+
+// decodeFindByTokenIDResponse is a transport/grpc.DecodeResponseFunc that converts
+// a gRPC concat reply to a user-domain concat response.
+func decodeFindByTokenIDResponse(_ context.Context, reply interface{}) (interface{}, error) {
+	return nil, errors.New("'Account' Decoder is not impelemented")
+}
