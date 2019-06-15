885b75248a188c0ad68ba4541895e77f370deeb8
huangwenwei <mr.huangwenwei@gmail.com>
2019-06-15 22:40:39 +0800

    Add lb to wallet_management client

---
+++ app/service/wallet_management/client/client.go
@@ -0,0 +1,68 @@
+package client
+
+import (
+  "io"
+  "fmt"
+  "time"
+  "github.com/go-kit/kit/log"
+  "trustkeeper-go/app/service/wallet_management/pkg/service"
+  walletmanagementEndpoint "trustkeeper-go/app/service/wallet_management/pkg/endpoint"
+  "github.com/go-kit/kit/endpoint"
+
+  sdconsul "github.com/go-kit/kit/sd/consul"
+  "github.com/go-kit/kit/sd/lb"
+  "github.com/go-kit/kit/sd"
+  "google.golang.org/grpc"
+  grpctransport "github.com/go-kit/kit/transport/grpc"
+  "trustkeeper-go/library/common"
+  libconsule "trustkeeper-go/library/consul"
+)
+
+// https://github.com/go-kit/kit/blob/master/examples/apigateway/main.go
+// https://github.com/go-kit/kit/blob/master/examples/profilesvc/client/client.go
+// https://github.com/go-kit/kit/blob/e2d71a06a40aa95cb82ccd72e854893612c02db7/sd/consul/integration_test.go
+func New(consulAddr string, logger log.Logger) (service.WalletManagementService, error) {
+  var (
+    consulTags = []string{"wallet"}
+    passingOnly = true
+    retryMax = 3
+    retryTimeout = 500 * time.Millisecond
+  )
+
+  consulClient, err := libconsule.NewClient(consulAddr)
+  if err != nil {
+    return nil, err
+  }
+
+  var (
+    sdClient = sdconsul.NewClient(consulClient)
+    registerSrvName = fmt.Sprintf("grpc.health.v1.%v", common.WalletManagementSrv)
+    instancer = sdconsul.NewInstancer(sdClient, logger, registerSrvName, consulTags, passingOnly)
+    endpoints = walletmanagementEndpoint.Endpoints{}
+  )
+  {
+    factory := factoryFor(walletmanagementEndpoint.MakeCreateChainEndpoint)
+    endpointer := sd.NewEndpointer(instancer, factory, logger)
+    balancer := lb.NewRoundRobin(endpointer)
+    retry := lb.Retry(retryMax, retryTimeout, balancer)
+    endpoints.CreateChainEndpoint = retry
+  }
+  return endpoints, nil
+}
+
+func factoryFor(makeEndpoint func(service.WalletManagementService) endpoint.Endpoint) sd.Factory {
+	return func(instance string) (endpoint.Endpoint, io.Closer, error) {
+    conn, err := grpc.Dial(instance, grpc.WithInsecure())
+    if err != nil {
+      return nil, nil, err
+    }
+
+    srv, err := newGRPCClient(conn, []grpctransport.ClientOption{})
+		if err != nil {
+			return nil, nil, err
+		}
+
+		endpoints := makeEndpoint(srv)
+    return endpoints, conn, err
+	}
+}
---
+++ app/service/wallet_management/client/grpc.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"fmt"
+	"context"
+	endpoint "github.com/go-kit/kit/endpoint"
+	grpc1 "github.com/go-kit/kit/transport/grpc"
+	grpc "google.golang.org/grpc"
+	endpoint1 "trustkeeper-go/app/service/wallet_management/pkg/endpoint"
+	pb "trustkeeper-go/app/service/wallet_management/pkg/grpc/pb"
+	service "trustkeeper-go/app/service/wallet_management/pkg/service"
+)
+
+// New returns an AddService backed by a gRPC server at the other end
+//  of the conn. The caller is responsible for constructing the conn, and
+// eventually closing the underlying transport. We bake-in certain middlewares,
+// implementing the client library pattern.
+func newGRPCClient(conn *grpc.ClientConn, options []grpc1.ClientOption) (service.WalletManagementService, error) {
+	var createChainEndpoint endpoint.Endpoint
+	{
+		createChainEndpoint = grpc1.NewClient(conn, "pb.WalletManagement", "CreateChain", encodeCreateChainRequest, decodeCreateChainResponse, pb.CreateChainReply{}, options...).Endpoint()
+	}
+
+	return endpoint1.Endpoints{CreateChainEndpoint: createChainEndpoint}, nil
+}
+
+// encodeCreateChainRequest is a transport/grpc.EncodeRequestFunc that converts a
+//  user-domain CreateChain request to a gRPC request.
+func encodeCreateChainRequest(_ context.Context, request interface{}) (interface{}, error) {
+	r, ok := request.(endpoint1.CreateChainRequest)
+	if !ok {
+		return nil, fmt.Errorf("request interface to endpoint.CreateChainRequest type assertion error")
+	}
+	return &pb.CreateChainRequest{
+		Symbol: r.Symbol,
+		Bitid: 	r.Bit44ID,
+    Status: r.Status}, nil
+}
+
+// decodeCreateChainResponse is a transport/grpc.DecodeResponseFunc that converts
+// a gRPC concat reply to a user-domain concat response.
+func decodeCreateChainResponse(_ context.Context, reply interface{}) (interface{}, error) {
+	_, ok := reply.(*pb.CreateChainReply)
+  if !ok{
+		e := fmt.Errorf("pb CreateChainReply type assertion error")
+    return &endpoint1.CreateChainResponse{Err: e}, e
+  }
+  return &endpoint1.CreateChainResponse{}, nil
+}
