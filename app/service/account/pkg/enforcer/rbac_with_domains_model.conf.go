c8edd1d3fae0b5eb34f95e5c52541019b7f2a55e
huangwenwei <mr.huangwenwei@gmail.com>
2019-06-20 17:53:26 +0800

    modify account casbin model configure

---
+++ app/service/account/pkg/enforcer/rbac_with_domains_model.conf.go
@@ -0,0 +1,20 @@
+package enforcer
+
+// https://www.cnblogs.com/wang_yb/archive/2018/11/20/9987397.html
+var CasbinConf = `
+[request_definition]
+r = sub, dom, obj, act
+
+[policy_definition]
+p = sub, dom, obj, act
+
+[role_definition]
+g = _, _, _
+
+[policy_effect]
+e = some(where (p.eft == allow))
+
+[matchers]
+m = g(r.sub, p.sub, r.dom) && r.dom == p.dom && r.obj == p.obj && r.act == p.act
+
+`
