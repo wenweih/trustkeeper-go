ddd106693d0c8244ba6825e1509057416e6dcae9
huangwenwei <mr.huangwenwei@gmail.com>
2019-07-30 19:41:11 +0800

    Fixed ethereum subHandle bug

--- app/service/chains_query/ledger_monitor/pub/ethereum.go
+++ app/service/chains_query/ledger_monitor/pub/ethereum.go
@@ -1,6 +1,9 @@
 package main
 
 import(
+  "strconv"
+  "errors"
+  "strings"
   "context"
   "math/big"
   "encoding/json"
@@ -12,7 +15,8 @@ func subHandle(orderHeight *big.Int, head *types.Header) (*big.Int, error) {
 	number := head.Number
   originBlock, err := svc.EthereumBlock(ctx, head.Number)
 	if err != nil {
-    logger.Log("FailToGetOriginBlock", err.Error, "Height", number.String())
+    e := errors.New(strings.Join([]string{"FailToGetOriginBlock ", err.Error(), " Height ", number.String()}, ""))
+    return big.NewInt(head.Number.Int64() - 1), e
 	}
 
 	if orderHeight.Cmp(big.NewInt(0)) == 0 {
@@ -25,28 +29,31 @@ func subHandle(orderHeight *big.Int, head *types.Header) (*big.Int, error) {
     originBlock.Number().Int64())
 
 	for blockNumber := orderHeight.Int64();
-    blockNumber <= originBlock.Number().Int64();
-    blockNumber++ {
-      orderHeight.Add(orderHeight, big.NewInt(1))
-      block, err := svc.EthereumBlock(ctx,  big.NewInt(blockNumber))
-  		if err != nil {
-        logger.Log("Get block error, height:", blockNumber)
-  			continue
-  		}
-  		body, err := json.Marshal(block)
-      if err != nil {
-        logger.Log("json Marshal raw ethereum block error", err.Error())
-        continue
+  blockNumber <= originBlock.Number().Int64();
+  blockNumber++ {
+    orderHeight.Add(orderHeight, big.NewInt(1))
+    block, err := svc.EthereumBlock(ctx,  big.NewInt(blockNumber))
+		if err != nil {
+      e := errors.New(strings.Join([]string{
+        "Get block error, height:",
+        strconv.FormatInt(blockNumber, 10)}, ""))
+			return big.NewInt(blockNumber), e
+		}
+		body, err := json.Marshal(block)
+    if err != nil {
+      e := errors.New(strings.Join([]string{"json Marshal raw ethereum block error", err.Error()}, ""))
+			return big.NewInt(blockNumber), e
+    }
+		if err := svc.MQPublish(
+      body,
+      "bestblock",
+      "fanout",
+      "ethereum",
+      "ethereum_best_block_queue");
+      err != nil {
+        e := errors.New(strings.Join([]string{"EtherumPublishError", err.Error()}, ""))
+        return big.NewInt(blockNumber), e
       }
-  		if err := svc.MQPublish(
-        body,
-        "bestblock",
-        "fanout",
-        "ethereum",
-        "ethereum_best_block_queue");
-        err != nil {
-          logger.Log("EtherumPublishError", err.Error())
-      }
-	}
-	return orderHeight, nil
+    }
+  return orderHeight, nil
 }
