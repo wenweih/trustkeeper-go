3124483357f97db1f2bb79fd30b95ad903c49d4a
huangwenwei <mr.huangwenwei@gmail.com>
2019-08-01 16:25:36 +0800

    add model for chains_query

---
+++ app/service/chains_query/pkg/model/balance.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+  "github.com/jinzhu/gorm"
+)
+
+// Balance balance related with assets for address
+type Balance struct {
+  gorm.Model
+  Address   string  `gorm:"unique_index:idx_address_symbol;not null"`
+  Symbol    string  `gorm:"unique_index:idx_address_symbol;not null"`
+  Identify  string
+  Decimal   uint64
+}
---
+++ app/service/chains_query/pkg/model/btc.go
@@ -0,0 +1,30 @@
+package model
+
+import(
+  "github.com/jinzhu/gorm"
+  "github.com/qor/transition"
+)
+
+// BtcUtxo utxo model
+type BtcUtxo struct {
+  gorm.Model
+  Txid                  string    `gorm:"not null"`
+  Amount                float64   `gorm:"not null"`
+  Height                int64     `gorm:"not null"`
+  VoutIndex             uint32    `gorm:"not null"`
+  ReOrg                 bool      `gorm:"not null;default:false"`
+  UsedBy                string
+  Balance               Balance   `gorm:"association_foreignkey:Address"`
+  BtcBlock              BtcBlock
+  BtcBlockID            uint
+  transition.Transition
+}
+
+// BtcBlock notify block info
+type BtcBlock struct {
+  gorm.Model
+  Hash    string    `gorm:"not null;unique_index:idx_hash_height"`
+  Height  int64     `gorm:"not null;unique_index:idx_hash_height"`
+  Utxos   []BtcUtxo `gorm:"foreignkey:BtcBlockID;association_foreignkey:Refer"`
+  ReOrg   bool      `gorm:"default:false"`
+}
