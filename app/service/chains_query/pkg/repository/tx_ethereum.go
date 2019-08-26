4ad8c6fe2d1b6c4baa3c8ddc2016b4883afcd141
huangwenwei <mr.huangwenwei@gmail.com>
2019-08-26 18:46:45 +0800

    Refactor ethereum tx construct

--- app/service/chains_query/pkg/repository/tx_ethereum.go
+++ app/service/chains_query/pkg/repository/tx_ethereum.go
@@ -7,62 +7,17 @@ import(
   "context"
   "strings"
   "math/big"
+  "github.com/ybbus/jsonrpc"
   "github.com/shopspring/decimal"
   "github.com/ethereum/go-ethereum/common"
-  "github.com/ybbus/jsonrpc"
   "trustkeeper-go/app/service/chains_query/pkg/model"
   "github.com/ethereum/go-ethereum/core/types"
 )
 
 func (repo *repo) ConstructTxETH(ctx context.Context, from, to, amount string) (string, string, error) {
-  // withdraw and deposit address validate
-  if !common.IsHexAddress(from) {
-    return "", "", fmt.Errorf("Invalid address: %s", from)
-  }
-  if !common.IsHexAddress(to) {
-    return "", "", fmt.Errorf("Invalid address: %s", to)
-  }
-
-  // balance data
-  balance := model.Balance{}
-  if err := repo.db.Where("address = ? AND symbol = ?", from, model.ETHSymbol).First(&balance).Error; err != nil {
-    return "", "", fmt.Errorf("Fail to query balance %s", err.Error())
-  }
-  balanceAmount, result := new(big.Int).SetString(balance.Amount, 10)
-  if !result {
-    return "", "", fmt.Errorf("fail to extract amount from balance")
-  }
-  withdrawLockAmount, result := new(big.Int).SetString(balance.WithdrawLock, 10)
-  if !result {
-    return "", "", fmt.Errorf("fail to extract withdrawLock from balance")
-  }
-  balanceAmount = balanceAmount.Sub(balanceAmount, withdrawLockAmount)
-  balanceAmountDecimal := decimal.NewFromBigInt(balanceAmount, 0)
-
-  // transfer amount data
-  transferAmountDecimal, err :=  decimal.NewFromString(amount)
+  balanceAmountDecimal, transferAmountDecimal, withdrawLockAmountDecimal, gasPrice, value, balance, err := repo.constructTxParamsValidate(ctx, from, to, amount, model.ETHSymbol)
   if err != nil {
-    return "", "", fmt.Errorf("Fail to extract transfer amount %s", err.Error())
-  }
-  decimalBig, result := new(big.Int).SetString(strconv.FormatUint(balance.Decimal, 10), 10)
-  if !result {
-    return "", "", fmt.Errorf("Fail to convert decimal")
-  }
-  transferAmountDecimal = transferAmountDecimal.Mul(decimal.NewFromBigInt(decimalBig, 0))
-  value, ok := new(big.Int).SetString(transferAmountDecimal.String(), 10)
-  if !ok {
-    return "", "", fmt.Errorf("Set amount error")
-  }
-
-  // check balance is whether GreaterThan transfer amount
-  if !balanceAmountDecimal.GreaterThan(transferAmountDecimal) {
-    return "", "", fmt.Errorf("Insufficient balance")
-  }
-
-  // gas price
-  gasPrice, err := repo.ethClient.SuggestGasPrice(ctx)
-  if err != nil {
-    return "", "", fmt.Errorf("Fail to query gas price %s", err.Error())
+    return "", "", err
   }
   // tx fee
   var (
@@ -81,7 +36,7 @@ func (repo *repo) ConstructTxETH(ctx context.Context, from, to, amount string) (
   if balanceAmountDecimal.LessThan(totalCost) {
     totalCostBig, _ := new(big.Int).SetString(totalCost.String(), 10)
     return "", "", fmt.Errorf("Insufficient ETH balance %s : %s",
-      model.ToEther(balanceAmount).String(),
+      model.ToEther(balanceAmountDecimal.Coefficient()).String(),
       model.ToEther(totalCostBig).String(),
     )
   }
@@ -136,7 +91,7 @@ func (repo *repo) ConstructTxETH(ctx context.Context, from, to, amount string) (
     return "", "", err
   }
   if err := repo.db.Model(&balance).
-  UpdateColumn("withdraw_lock", decimal.NewFromBigInt(withdrawLockAmount, 0).Add(decimal.NewFromBigInt(value, 0)).String()).Error;
+  UpdateColumn("withdraw_lock", withdrawLockAmountDecimal.Add(decimal.NewFromBigInt(value, 0)).String()).Error;
   err != nil {
     return "", "", fmt.Errorf("Fail to update withdrawLock row %s", err.Error())
   }
@@ -179,6 +134,10 @@ func (repo *repo) SendETHTx(ctx context.Context, signedTxHex string) (txID strin
   return tx.Hash().String(), nil
 }
 
+func (repo *repo) ConstructTxERC20(ctx context.Context, from, to, amount, contract string) (unsignedTxHex, chainID string, err error) {
+  return "", "", nil
+}
+
 func (repo *repo) rollbackETHTx(ms types.Message) error {
   sender := ms.From().String()
   balance := model.Balance{}
@@ -195,4 +154,60 @@ func (repo *repo) rollbackETHTx(ms types.Message) error {
     return fmt.Errorf("Fail to update withdrawLock row %s", err.Error())
   }
   return nil
+}
+
+func (repo *repo) constructTxParamsValidate(ctx context.Context, from, to, amount, symbol string) (balanceAmountDecimal,
+  transferAmountDecimal, withdrawLockAmountDecimal decimal.Decimal, gasPrice *big.Int, transferValue *big.Int, balance model.Balance, rr error){
+  // withdraw and deposit address validate
+  if !common.IsHexAddress(from) {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Invalid address: %s", from)
+  }
+  if !common.IsHexAddress(to) {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Invalid address: %s", to)
+  }
+
+  // balance data
+  balance = model.Balance{}
+  err := repo.db.Where("address = ? AND symbol = ?", from, model.ETHSymbol).First(&balance).Error
+  if err != nil {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Fail to query balance %s", err.Error())
+  }
+  balanceAmount, result := new(big.Int).SetString(balance.Amount, 10)
+  if !result {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("fail to extract amount from balance")
+  }
+  withdrawLockAmount, result := new(big.Int).SetString(balance.WithdrawLock, 10)
+  if !result {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("fail to extract withdrawLock from balance")
+  }
+  withdrawLockAmountDecimal = decimal.NewFromBigInt(withdrawLockAmount, 0)
+  balanceAmount = balanceAmount.Sub(balanceAmount, withdrawLockAmount)
+  balanceAmountDecimal = decimal.NewFromBigInt(balanceAmount, 0)
+
+  // transfer amount data
+  transferAmountDecimal, err =  decimal.NewFromString(amount)
+  if err != nil {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Fail to extract transfer amount %s", err.Error())
+  }
+  decimalBig, result := new(big.Int).SetString(strconv.FormatUint(balance.Decimal, 10), 10)
+  if !result {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Fail to convert decimal")
+  }
+  transferAmountDecimal = transferAmountDecimal.Mul(decimal.NewFromBigInt(decimalBig, 0))
+  transferValue, ok := new(big.Int).SetString(transferAmountDecimal.String(), 10)
+  if !ok {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Set amount error")
+  }
+
+  // check balance is whether GreaterThan transfer amount
+  if !balanceAmountDecimal.GreaterThan(transferAmountDecimal) {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Insufficient balance")
+  }
+
+  // gas price
+  gasPrice, err = repo.ethClient.SuggestGasPrice(ctx)
+  if err != nil {
+    return decimal.Decimal{}, decimal.Decimal{}, decimal.Decimal{}, nil, nil, model.Balance{}, fmt.Errorf("Fail to query gas price %s", err.Error())
+  }
+  return
 }
